f133492a2331fb92fd3b16b36d430f09aa39fee8
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2011-10-03 14:19:41 +0000

    a couple of new controller classes, incomplete. Also, modified pom.xml and some other config files.

--- src/main/java/org/citizensense/controller/CampaignController.java
+++ src/main/java/org/citizensense/controller/CampaignController.java
@@ -2,9 +2,12 @@ package org.citizensense.controller;
 
 import org.citizensense.model.Campaign;
 import com.opensymphony.xwork2.ModelDriven;
+import org.apache.struts2.rest.DefaultHttpHeaders;
+import org.apache.struts2.rest.HttpHeaders;
 
 public class CampaignController implements ModelDriven<Campaign>{
 	
+	Campaign camp = new Campaign();
 	/**
 	 * @param args
 	 */
@@ -16,7 +19,15 @@ public class CampaignController implements ModelDriven<Campaign>{
 	@Override
 	public Campaign getModel() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.camp;
+	}
+	
+	public HttpHeaders show() {
+		return new DefaultHttpHeaders("show");
+	}
+	
+	public HttpHeaders index() {
+		return new DefaultHttpHeaders("index");
 	}
 
 }
