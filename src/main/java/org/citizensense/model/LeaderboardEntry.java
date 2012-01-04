d32d9c3f390cbe075b0791bae243932c019fd752
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2012-01-04 20:50:13 +0000

    update LeaderboardEntry to sort by points

--- src/main/java/org/citizensense/model/LeaderboardEntry.java
+++ src/main/java/org/citizensense/model/LeaderboardEntry.java
@@ -1,37 +1,42 @@
 package org.citizensense.model;
 
-public class LeaderboardEntry implements Comparable<LeaderboardEntry>{
+public class LeaderboardEntry implements Comparable<LeaderboardEntry> {
 	private String name;
 	private int id;
 	private int points;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getPoints() {
 		return points;
 	}
+
 	public void setPoints(int points) {
 		this.points = points;
 	}
-	
+
 	@Override
 	public int compareTo(LeaderboardEntry arg0) {
-		if(this.getPoints() < arg0.getPoints())
+		if (this.getPoints() < arg0.getPoints())
 			return -1;
-		else if(this.getPoints() > arg0.getPoints())
+		if (this.getPoints() > arg0.getPoints())
 			return 1;
-		else
-			return 0;
+
+		return 0;
 	}
-	
+
 }
