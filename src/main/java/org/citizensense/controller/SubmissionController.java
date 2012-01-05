53e0bff9048260e5c799828608fe4f129a30125e
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2012-01-05 04:27:28 +0000

    Submission view/url code

---
+++ src/main/java/org/citizensense/controller/SubmissionController.java
@@ -0,0 +1,60 @@
+package org.citizensense.controller;
+
+
+import java.util.Collection;
+
+import org.citizensense.model.Submission;
+import org.citizensense.util.*;
+
+import com.opensymphony.xwork2.ModelDriven;
+import org.apache.struts2.rest.DefaultHttpHeaders;
+import org.apache.struts2.rest.HttpHeaders;
+
+public class SubmissionController implements ModelDriven<Object>{
+	
+
+	private Collection<Submission> list;
+	private int id;
+	private Submission task = new Submission();
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	@Override
+	public Object getModel() {
+		return (list != null ? list : task);
+	}
+	
+	public HttpHeaders show() {
+		return new DefaultHttpHeaders("show");
+	}
+	
+	public void setId(String id) {
+		if (id != null)
+			this.task = SubmissionService.getSubmissions().get(Integer.parseInt(id)-1);
+		this.id = Integer.parseInt(id);		
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public HttpHeaders index() {
+		list = SubmissionService.getSubmissions();
+		return new DefaultHttpHeaders("index").disableCaching();
+	}
+	
+	public HttpHeaders create()
+	{
+		SubmissionService.save(task);
+		return new DefaultHttpHeaders("create");
+	}
+
+}
---
+++ src/main/java/org/citizensense/util/SubmissionService.java
@@ -0,0 +1,68 @@
+package org.citizensense.util;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.citizensense.model.*;
+import org.hibernate.Session;
+
+
+public class SubmissionService {
+
+	
+	public static List<Submission> getSubmissions() {
+		List<Submission> submissions;
+
+		Session session = HibernateFactory.getSessionFactory().getCurrentSession();
+		//Session session = HibernateFactory.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		
+		submissions = session.createQuery("from Submission").list();
+//		for(Task t : tasks) {
+//			submissions = session.createQuery("from Task as t join from ").list();
+//			t.setSubmissions(submissions);
+//		}
+		session.getTransaction().commit();
+		
+//		for(Task t: submissions) {
+//			getSubmissions(t);
+//			getQuestions(t);
+//		}
+		
+		return submissions;
+	}
+	
+//	public static void getSubmissions(Task t) {
+//		List<Submission> submissions;
+//		Session session = HibernateFactory.getSessionFactory().getCurrentSession();
+//		session.beginTransaction();
+//		
+//		submissions = session.createQuery("from Submission where task_id= " + t.getId()).list();
+//		
+//		t.setSubmissions(submissions);
+//		
+//		session.getTransaction().commit();
+//	}
+//	
+//	public static void getQuestions(Task t) {
+//		List<Question> questions;
+//		Session session = HibernateFactory.getSessionFactory().getCurrentSession();
+//		session.beginTransaction();
+//		
+//		questions = session.createQuery("from Question where task_id= " + t.getId()).list();
+//		
+//		Collections.sort(questions);
+//		t.setQuestions(questions);
+//		
+//		session.getTransaction().commit();
+//	}
+
+	public static void save(Submission t) {
+		Session session = HibernateFactory.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		session.save(t);
+		session.getTransaction().commit();
+		
+	}
+
+}
