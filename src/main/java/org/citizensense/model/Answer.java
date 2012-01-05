0e2111aca87e72542396b221e17ce457599297e3
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2012-01-05 03:31:22 +0000

    removed inheritance pattern

--- src/main/java/org/citizensense/model/Answer.java
+++ src/main/java/org/citizensense/model/Answer.java
@@ -1,10 +1,49 @@
 package org.citizensense.model;
 
-public interface Answer {
-public String getType();
-public void setType(String type);
-public String getResponse();
-public void setResponse(String res);
-public Integer getTaskId();
-public void setTaskId(Integer id);
+public class Answer {
+	private int id;
+	private String answer;
+	private String type;
+	private int q_id;
+	private int sub_id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getQ_id() {
+		return q_id;
+	}
+	public void setQ_id(int q_id) {
+		this.q_id = q_id;
+	}
+	public int getSub_id() {
+		return sub_id;
+	}
+	public void setSub_id(int sub_id) {
+		this.sub_id = sub_id;
+	}
+	
+	
+//	
+//public String getType();
+//public void setType(String type);
+//public String getResponse();
+//public void setResponse(String res);
+//public Integer getTaskId();
+//public void setTaskId(Integer id);
 }
