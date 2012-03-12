a4c79fd960728e4441de8773c4a45d63a3425f26
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2012-03-12 18:58:42 +0000

    fixes a lot of the things listed in the Alpha comments

--- src/com/citizensense/android/CitizenSense.java
+++ src/com/citizensense/android/CitizenSense.java
@@ -7,6 +7,7 @@ package com.citizensense.android;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
