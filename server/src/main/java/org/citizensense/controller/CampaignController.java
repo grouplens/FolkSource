b29fca2be71505581592087d523d053226e8f9e0
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2012-08-22 17:26:37 +0000

    try out Access-Control-Allow-Origin in campaign index

--- server/src/main/java/org/citizensense/controller/CampaignController.java
+++ server/src/main/java/org/citizensense/controller/CampaignController.java
@@ -1,10 +1,14 @@
 package org.citizensense.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.citizensense.model.Campaign;
 import org.citizensense.util.*;
 import org.grouplens.common.dto.DtoContainer;
 
 import com.opensymphony.xwork2.ModelDriven;
+
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.rest.DefaultHttpHeaders;
 import org.apache.struts2.rest.HttpHeaders;
 
@@ -43,7 +47,9 @@ public class CampaignController implements ModelDriven<DtoContainer<Campaign>>{
 		//list = CampaignService.getCampaigns();
 		//return new DefaultHttpHeaders("index").disableCaching();
 		content = new DtoContainer<Campaign>(Campaign.class, true);
+		HttpServletResponse res = ServletActionContext.getResponse();
 		content.set(CampaignService.getCampaigns());
+		res.addHeader("Access-Control-Allow-Origin", "*");
 		return "index";
 	}
 	
