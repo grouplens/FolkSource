99df57812a89774e316f3a971ff724defd6f874a
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2013-04-30 15:03:48 +0000

    add menupane lib back

---
+++ bootplate_22/source/menupane/Toolbar.js
@@ -0,0 +1,25 @@
+enyo.kind({
+  name: "Toolbar",
+  kind: "onyx.Toolbar",
+  published: {
+    header: ""
+  },
+  events: {
+    onHeader: "",
+    onToggleMenu: "",
+    onToggleSecondaryMenu: ""
+  },
+  components: [
+    { kind: "onyx.Grabber", ontap: "doToggleMenu", style: "float: left;" },
+    { name: "header", content: "", ontap: "doHeader", style: "padding-top: 4px" },
+  ],
+
+  create: function() {
+    this.inherited(arguments);
+    this.headerChanged();
+  },
+
+  headerChanged: function() {
+    this.$.header.setContent(this.getHeader());
+  }
+});
---
+++ bootplate_22/source/menupane/package.js
@@ -0,0 +1,4 @@
+enyo.depends(
+	"MenuPane.js",
+    "Toolbar.js"
+);
