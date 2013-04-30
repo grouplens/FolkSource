ef4fe6b3e5e98b6ca0b60e155b3c37b005b70fe2
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2013-04-30 15:03:48 +0000

    add menupane lib back

---
+++ source/menupane/examples/both/package.js
@@ -0,0 +1,15 @@
+/* package.js */
+enyo.depends(
+  // requires layout lib for Slideable 
+  "$lib/layout",
+  // requires onyx lib for Toolbar
+  "$lib/onyx",
+
+  // require menupane kind
+	"../../",
+
+  // example code
+  "Toolbar.js",
+	"App.js",
+	"style.css"
+);
