ce6e15af3a78ce923ccc0dc7d273c0cd51c8df5a
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2013-08-05 16:49:29 +0000

    fix the bug that was blocking Noah (FilledPanels not working properly)

--- bootplate_22/source/FilledPanels.js
+++ bootplate_22/source/FilledPanels.js
@@ -11,67 +11,67 @@ enyo.kind({
         onDoObservation: "hidePopup"
     },
     components: [
-	{name: "mapUp", kind: "onyx.Popup", style: "width: 80%; position: fixed; z-index: 2;", classes: "onyx-popup", centered: true, floating: true, modal: true, components: [
-		{name: "pview", kind: "PinView", classes: "mapHide"}
-	]},
-	{kind: enyo.FittableColumns, fit: true, components: [
-		{name: "leftButton", kind: "onyx.Button", content: "<", slide: "prev", ontap: "buttonTapHandler", classes: "filledButtons", disabled: !0},
-    		{name: "panels", kind: "Panels", arrangerKind: "CarouselArranger", onTransitionFinish: "transitionFinishHandler", onTransitionStart: "transitionStartHandler",classes: "filledPanels", layoutKind: "enyo.FittableColumnsLayout", fit: true, components: []},
-    		{name: "rightButton", kind: "onyx.Button", content: ">", slide: "next", ontap: "buttonTapHandler", classes: "filledButtons"}, 
-	]},
-    	{kind: "Signals", onPinClicked: "popupTriggered"}
+    	{kind: "Signals", onPinClicked: "popupTriggered"},
+		{name: "mapUp", kind: "onyx.Popup", style: "width: 80%; position: fixed; z-index: 2;", classes: "onyx-popup", centered: true, floating: true, modal: true, components: [
+			{name: "pview", kind: "PinView", classes: "mapHide"}
+		]},
+		{kind: enyo.FittableColumns, fit: true, components: [
+			{name: "leftButton", kind: "onyx.Button", content: "<", slide: "prev", ontap: "buttonTapHandler", classes: "filledButtons", disabled: !0},
+			{name: "panels", kind: "Panels", arrangerKind: "CarouselArranger", onTransitionFinish: "transitionFinishHandler", onTransitionStart: "transitionStartHandler", classes: "filledPanels", layoutKind: "enyo.FittableColumnsLayout", fit: true},
+			{name: "rightButton", kind: "onyx.Button", content: ">", slide: "next", ontap: "buttonTapHandler", classes: "filledButtons"}, 
+		]},
     ],
     create: function (a, b) {
-	this.inherited(arguments);
-	//this.$.spinnerUp.show();
-	//this.$.mapUp.show();
-	var c = Data.getURL() + "campaign.json";
-	var d = new enyo.Ajax({method: "GET", cacheBust: false, url: c, handleAs: "json"});
-	d.go().response(this, "renderResponse");; 
-	this.$.panels.$.animator.setDuration(350);
-    },
-    renderResponse: function (a, b) {
-        this.campaignArray = b.campaigns;
-	this.log(this.campaignArray);
-        for (var c in this.campaignArray) {
-            var currentCampaign = this.campaignArray[c];
-            var e = "panel_" + currentCampaign.id;
-            var f = "item_" + currentCampaign.id;
-            var g = "map_" + currentCampaign.id;
-	    /*
-	     * This block below is needed because not all browsers parse dates
-	     * the same way. This should be mostly browser compatible.
-	     */
-	    var date = Date.parse(new Date());
-	    var st = currentCampaign.start_date_string.split(/[- :]/);
-	    var en = currentCampaign.end_date_string.split(/[- :]/);
-	    var startDate = Date.parse(new Date(st[0], st[1]-1, st[2], st[3], st[4], st[5]));
-	    var endDate = Date.parse(new Date(en[0], en[1]-1, en[2], en[3], en[4], en[5]));
+		this.inherited(arguments);
+		//this.$.spinnerUp.show();
+		//this.$.mapUp.show();
+		var c = Data.getURL() + "campaign.json";
+		var d = new enyo.Ajax({method: "GET", cacheBust: false, url: c, handleAs: "json"});
+		d.go().response(this, "renderResponse");; 
+		this.$.panels.$.animator.setDuration(350);
+	},
+	renderResponse: function (a, b) {
+		this.campaignArray = b.campaigns;
+		this.log(this.campaignArray);
+		for (var c in this.campaignArray) {
+			var currentCampaign = this.campaignArray[c];
+			var e = "panel_" + currentCampaign.id;
+			var f = "item_" + currentCampaign.id;
+			var g = "map_" + currentCampaign.id;
+			/*
+			 * This block below is needed because not all browsers parse dates
+			 * the same way. This should be mostly browser compatible.
+			 */
+			var date = Date.parse(new Date());
+			var st = currentCampaign.start_date_string.split(/[- :]/);
+			var en = currentCampaign.end_date_string.split(/[- :]/);
+			var startDate = Date.parse(new Date(st[0], st[1]-1, st[2], st[3], st[4], st[5]));
+			var endDate = Date.parse(new Date(en[0], en[1]-1, en[2], en[3], en[4], en[5]));
 
-	    //if(endDate >= date) { // "closed" campaigns shouldn't show up
-		this.$.panels.createComponent(
-		    {name: e, classes: "panelItem", fit: true, kind: "enyo.FittableRows", components: [
-			{name: f, kind: "CampaignItem", title: "" + currentCampaign.title, description: "" + currentCampaign.description},
-			{name: g, fit: true, kind: "NewMap", /*layoutKind: "enyo.FittableColumnsLayout", */provider: "openlayers", style: "height: 100%; width: 100%;" /*overflow: hidden;"*/}
-			//{name: g, fit: true, kind: "MapStraction", /*layoutKind: "enyo.FittableColumnsLayout", */provider: "openlayers", style: "height: 100%; width: 100%;" /*overflow: hidden;"*/}
-		]});
-		/*if(date < startDate) { // setup structure for disallowing campaigns to show up/be used before they're "open"
-		    this.log(this.$.panels.$[e]);
-		    this.$.panels.$[e].running = false;
+			//if(endDate >= date) { // "closed" campaigns shouldn't show up
+			this.$.panels.createComponent(
+				{name: e, classes: "panelItem", /*fit: true,*/ kind: enyo.FittableRows, components: [
+					{name: f, kind: "CampaignItem", title: "" + currentCampaign.title, description: "" + currentCampaign.description},
+					{name: g, fit: true, kind: "NewMap", /*layoutKind: "enyo.FittableColumnsLayout", */provider: "openlayers", style: "height: 100%; width: 100%;" /*overflow: hidden;"*/}
+					//{name: g, fit: true, kind: "MapStraction", /*layoutKind: "enyo.FittableColumnsLayout", */provider: "openlayers", style: "height: 100%; width: 100%;" /*overflow: hidden;"*/}
+			]});
+			/*if(date < startDate) { // setup structure for disallowing campaigns to show up/be used before they're "open"
+			  this.log(this.$.panels.$[e]);
+			  this.$.panels.$[e].running = false;
+			  }
+			  }*/
+			this.render();
+			this.checkSides(); // make sure the arrow buttons work
 		}
-	    }*/
-	    this.render();
-	    this.checkSides(); // make sure the arrow buttons work
-        }
-    },
-    buttonTapHandler: function (a, b) {
-        if(a.slide === "prev")
-	    this.$.panels.previous();
-	else if(a.slide === "next")
-	    this.$.panels.next();
-	else 
-	    this.$.panels.snapTo(a.slide);
-    },
+	},
+	buttonTapHandler: function (a, b) {
+		if(a.slide === "prev")
+			this.$.panels.previous();
+		else if(a.slide === "next")
+			this.$.panels.next();
+		else 
+			this.$.panels.snapTo(a.slide);
+	},
     transitionFinishHandler: function (a, b) {
         var c = this.$.panels.getIndex();
         this.waterfall("onSnapped", undefined, c);
@@ -84,7 +84,7 @@ enyo.kind({
     checkSides: function () {
         var index = this.$.panels.getIndex();
         var size = this.$.panels.getPanels().length;
-	var adjSize = size - 1; //adjust for counting at 0 vs. 1
+		var adjSize = size - 1; //adjust for counting at 0 vs. 1
         if(size == 1) {
             this.$.rightButton.setDisabled(true);
             this.$.leftButton.setDisabled(true);
