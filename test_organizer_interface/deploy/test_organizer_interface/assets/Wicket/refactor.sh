546a89f3115339610bbceb6b0c68121fa9b93609
thebault <thebault@e6cc3703-4d0c-0410-8df7-e55f7143975b>
2013-12-04 21:25:04 +0000

    clean up some
    Former-commit-id: 2e7320355df18a48e6eea78b9a763b253ed493c7

---
+++ test_organizer_interface/deploy/test_organizer_interface/assets/Wicket/refactor.sh
@@ -0,0 +1,4 @@
+java -jar ./compiler-latest/compiler.jar --compilation_level WHITESPACE_ONLY --js wicket-gmap3.src.js --js_output_file wicket-gmap3.js
+java -jar ./compiler-latest/compiler.jar --compilation_level WHITESPACE_ONLY --js wicket-leaflet.src.js --js_output_file wicket-leaflet.js
+java -jar ./compiler-latest/compiler.jar --compilation_level WHITESPACE_ONLY --js wicket-arcgis.src.js --js_output_file wicket-arcgis.js
+java -jar ./compiler-latest/compiler.jar --compilation_level WHITESPACE_ONLY --js wicket.src.js --js_output_file wicket.js
