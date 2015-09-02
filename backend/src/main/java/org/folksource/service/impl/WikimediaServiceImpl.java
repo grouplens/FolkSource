0bf1694ec3c01801627c937662ca8d6c46dd231e
Tyler Danielson <tysdanielson@gmail.com>
2015-09-02 12:08:56 -0500

    latest updates for oauth

--- backend/src/main/java/org/folksource/service/impl/WikimediaServiceImpl.java
+++ backend/src/main/java/org/folksource/service/impl/WikimediaServiceImpl.java
@@ -3,7 +3,9 @@ package org.folksource.service.impl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.security.Key;
 
+import javax.crypto.SecretKeyFactory;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Feature;
@@ -23,6 +25,12 @@ import org.glassfish.jersey.client.oauth1.AccessToken;
 import org.glassfish.jersey.client.oauth1.ConsumerCredentials;
 import org.glassfish.jersey.client.oauth1.OAuth1AuthorizationFlow;
 import org.glassfish.jersey.client.oauth1.OAuth1ClientSupport;
+import org.jose4j.jwe.JsonWebEncryption;
+import org.jose4j.jwt.JwtClaims;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.jose4j.jwt.consumer.JwtConsumer;
+import org.jose4j.jwt.consumer.JwtConsumerBuilder;
+import org.jose4j.lang.JoseException;
 
 public class WikimediaServiceImpl implements WikimediaService {
 	final static Logger logger = LoggerFactory.getLogger(WikimediaServiceImpl.class);
@@ -52,12 +60,14 @@ public class WikimediaServiceImpl implements WikimediaService {
 	@Transactional
 	public String verify(String verifier){
 		OAuth1AuthorizationFlow authFlow = this.getAuthFlow();
+		String wikiUrl = serviceHelper.getAppProperties().getProperty("wiki.url");
+		String consumerKey = serviceHelper.getAppProperties().getProperty("wiki.key");
 		
 		AccessToken accessToken = authFlow.finish(verifier);
 		Feature feature = authFlow.getOAuth1Feature();
 		Client client = ClientBuilder.newBuilder().register(feature).build();
 		
-		Response resp = client.target("http://localhost:5555/mediawiki/index.php?title=Special%3AOAuth%2Fidentify").request().get();
+		Response resp = client.target(wikiUrl + "index.php?title=Special%3AOAuth%2Fidentify").request().get();
 		
 		
 		StringWriter writer = new StringWriter();
@@ -68,6 +78,28 @@ public class WikimediaServiceImpl implements WikimediaService {
 			e.printStackTrace();
 		}
 		String thestring = writer.toString();
+		
+		JwtConsumer jwtConsumer = new JwtConsumerBuilder()
+	            .setRequireExpirationTime() // the JWT must have an expiration time
+	            .setAllowedClockSkewInSeconds(30) // allow some leeway in validating time based claims to account for clock skew
+	            .setRequireSubject() // the JWT must have a subject claim
+	            .setExpectedIssuer("Issuer") // whom the JWT needs to have been issued by
+	            .setExpectedAudience("Audience") // to whom the JWT is intended for
+	            //.setVerificationKey(factory) // verify the signature with the public key
+	            .build(); 
+		
+		try
+	    {
+	        //  Validate the JWT and process it to the Claims
+	        JwtClaims jwtClaims = jwtConsumer.processToClaims(thestring);
+	        System.out.println("JWT validation succeeded! " + jwtClaims);
+	    }
+	    catch (InvalidJwtException e)
+	    {
+	        // InvalidJwtException will be thrown, if the JWT failed processing or validation in anyway.
+	        // Hopefully with meaningful explanations(s) about what went wrong.
+	        System.out.println("Invalid JWT! " + e);
+	    }
 		//User user = userDao.find(1);
 		//user.setName("Tyler");
 		
